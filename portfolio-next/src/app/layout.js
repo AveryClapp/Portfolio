97b1e1c36944691cb9bc665100625e940c2b1e94
AveryClapp <avery.clapp@gmail.com>
2026-01-28 12:11:11 -0500

    cleanup

--- portfolio-next/src/app/layout.js
+++ portfolio-next/src/app/layout.js
@@ -2,7 +2,6 @@ import "@/app/globals.css";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
 import { ViewTransitions } from "next-view-transitions";
-import LinkInterceptor from "@/Components/ViewTransitions/LinkInterceptor";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,7 +22,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} bg-stone-100`}>
       <body className="bg-stone-100 min-h-screen">
-        {/* <LinkInterceptor /> */}
         <ViewTransitions>
           <Suspense fallback={null}>
             {children}
